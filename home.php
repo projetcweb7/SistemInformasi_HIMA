0b1ca74a4bc279ff8db24e7a50519d693dc90f06
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-23 21:22:03 +0700

    Add files via upload
    Upload File pertama

---
+++ home.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+  <!-- =======================================================
+    Theme Name: Vesperr
+    Theme URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+	======================================================= -->
+</head>
+
+<body>
+  <audio src="assets/img/ab.mp3" autoplay="autoplay" hidden="hidden" controls="loop"></audio>
+  <header>
+    <!-- start top -->
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+            <a href="home.php"><img src="assets/img/pcr1.png" height="100px" width="100px" alt="" /></a>
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li class="current"><a href="home.php">Beranda</a></li>
+                <li><a href="tentang.php" class="external">Tentang</a></li>
+                <li class="dropdown">
+                  <a href="#"> Himpunan <i class="icon-angle-down"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="itsa.php" class="external">ITSA</a></li>
+                    <li><a href="himasistifo.php" class="external">Himasistifo</a></li>
+                    <li><a href="himakom.php" class="external">Himakom</a></li>
+                    <li><a href="himika.php" class="external">Himika</a></li>
+                    <li><a href="aet.php" class="external">AET</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+  <!-- section intro -->
+  <section id="intro">
+    <div class="slogan">
+      <div class="icon">
+        <img src="assets/img/pcr3.png">
+       <!--  <i class="icon-beaker i"></i> -->
+      </div>
+      <h1>Welcome to <span>Website </span> Informasi</h1>
+      <br/><h1> Himpunan Mahasiswa</h1>
+      <h2>Politeknik Caltex Riau</h2>
+    </div>
+  </section>
+  <!-- end intro -->
+  <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <h3><span>Himpunan Mahasiswa</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>
+               HIMA adalah organisasi mahasiswa tingkat jurusan/fakultas di suatu perguruan tinggi. Namun ternyata terdapat perbedaan antara HIMA di Politeknik Caltex Riau dengan HIMA di Perguruan Tinggi lain. Bila di perguruan tinggi HIMA dibentuk dalam setiap jurusan/fakultas, namun di Politeknik AKA HIMA dibentuk dalam setiap Prodi yaitu HIMA ITSA, Himasistifo, Himakom, Himatek, Himika, AET, Himatron, Himaksi, Himatrik, HMM yang terbagi dari 3 jurusan yaitu : Teknologi Industri, Teknologi Informasi, Akuntansi.</p><br>
+<p>
+Perubahan sistem kampus dari Akademi menjadi Politeknik merupakan salah satu hal yang mendasari terbentuknya HIMA. Perubahan sistem ini membuat Politeknik Caltex Riau memiliki 10 prodi, dan untuk mewakili setiap prodi itu harus memiliki wadah organisasi yang legal sehingga dibentuklah badan hukum untuk mengatur kelegalan organisasi tersebut. Selain itu telah disebarkan kuisioner dari Presma tentang pembentukan HIMA ini dan rata-rata mahasiswa mengatakan perlu adanya HIMA di Politeknik Caltex Riau.
+            </p>
+             <div class="heading">
+            <h3><span>Berikut beberapa Hima di PCR : </span></h3>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="span3">
+          <img src="assets/img/dummies/itsa1.jpg" width="350px" height="350px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h5><strong>ITSA</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/sis3.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h5><strong>Hima Sistifo</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/himkom.jpg" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h5><strong>Himakom</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/Aet.jpg" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h5><strong>AET</strong></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end section about -->
+  <!-- section services -->
+
+  <!-- end section contact -->
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand"><img src="assets/img/pcr1.png" alt="" /></a>
+              </div>
+              <p>
+                &copy; AJI Team - All right reserved
+              </p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Vesperr
+                -->
+                Created by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="scrollup"><i class="icon-chevron-up icon-square icon-48 active"></i></a>
+
+  <!-- Javascript Library Files -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.easing.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/parallax/jquery.parallax-1.1.3.js"></script>
+  <script src="assets/js/nagging-menu.js"></script>
+  <script src="assets/js/jquery.nav.js"></script>
+  <script src="assets/js/prettyPhoto/jquery.prettyPhoto.js"></script>
+  <script src="assets/js/portfolio/jquery.quicksand.js"></script>
+  <script src="assets/js/portfolio/setting.js"></script>
+  <script src="assets/js/hover/jquery-hover-effect.js"></script>
+  <script src="assets/js/jquery.scrollTo.min.js"></script>
+  <script src="assets/js/animate.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Custom Javascript File -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
