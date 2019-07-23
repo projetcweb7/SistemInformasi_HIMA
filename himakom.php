667aef5d26cd6be6e77973f11a3730e0075de55e
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-23 22:13:07 +0700

    Create himakom.php
    Buat file baru himakom

---
+++ himakom.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	 		<p>merupakan himpunan mahasiswa yang beranggotakan mahasiswa aktif dan alumni yang berasal dari Program Studi Teknik Komputer. HIMAKOM dibentuk untuk membantu setiap anggotanya dalam mengatasi persoalan edukasi dan sosial diluar maupun di dalam kampus. </p><br>
+            <p> <strong>SEJARAH : </strong></p>
+            <p>Himpunan mahasiswa Program Studi Teknik Komputer ini berdiri Pada Tahun 2001 yang dibentuk oleh mahasiswa Teknik Komputer.</p>
+            <p><strong>VISI : </strong></p>
+            <p>Diakui sebagai program studi unggulan di bidang Teknik Komputer dan Teknologi Informasi yang dapat bersaing secara global.</p>
+
+            <p><strong>MISI :</strong></p>
+            <p>1. Menyelenggarakan Sistem Pendidikan Profesional di bidang Teknik Komputer dan Teknologi Informasi yang Menghasilkan Sumber Daya Manusia, Barang (software dan hardware) dan Jasa (konsultasi ) yang berkualitas.</p>
+            <p>2. Menghasilkan lulusan yang profesional/terampil dan ahli di bidang Teknik Komputer dan Teknologi Informasi, berpikir terbuka (open minded) serta siap bersaing di pasar global berdasarkan kompetensi dunia Industri pada tingkat Nasional maupun Internasional dengan menyediakan sebuah lingkungan belajar yang baik bagi mahasiswa.</p>
+            <p>3. Ikut berperan aktif dalam pengembangan asosiasi profesi di bidang komputer dan teknologi informasi yang menunjang kelancaran hubungan institusional dengan masyarakat industri. Mengembangkan dan menerapkan nilai-nilai etika, moral agama dan moral akademis.</p>
+            <p>4. Mengembangkan Riset terapan yang terkait dengan bidang komputer dan Teknologi Informasi untuk melayani kebutuhan industri yang meliputi kebutuhan sumber daya manusia, konsultasi teknis dan penelitian.</p>
+
+</body>
+</html>
