29faba3f546ed725b2f4f7ad71ddb32a63915fdb
Jean <52515611+jean-aja@users.noreply.github.com>
2019-07-23 23:09:19 +0700

    upload_himika.php

---
+++ himika.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+</head>
+ <body>
+  <header>
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+        <img src="assets/img/pcr1.png" height="100px" width="100px" alt="" />
+        </a>
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li><a href="home.php" class="external">Beranda</a></li>
+                <li><a href="tentang.php" class="external">Tentang</a></li>
+                <li class="dropdown current">
+                  <a href="#">Himpunan<i class="icon-angle-down"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="itsa.php" class="external">ITSA</a></li>
+                    <li><a href="himasistifo.php" class="external">Himasistifo</a></li>
+                    <li><a href="himakom.php" class="external">Himakom</a></li>
+                    <li><a href="himika.php" class="external">Himika</a></li>
+                    <li><a href="aet.php" class="external">AET</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+           <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+
+            <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <img src="assets/img/dummies/asa.jpg" height="100" width="100">
+            <h3><span>Himika</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>HIMIKA (Himpunan Mahasiswa Elektronika) Politeknik Caltex Riau merupakan lembaga himpunan mahasiswa/i Teknik Elektronika di Politeknik Caltex Riau. HIMIKA didirikan di kampus Politeknik Caltex Riau pada tanggal 15 Mei 2001 berkedudukan di kesekretariat HIMIKA Kampus Politeknik Caltex Riau(PCR) Rumbai, Pekanbaru. </p><br>
+
+            <p><strong>VISI : </strong></p>
+            <p>1. Menjadikan Himika Menjadi Hima No 1 Di Politeknik Caltex Riau.</p>
+            <p>2. Menjadikan Seluruh Penghuni Himika : Pengurus, Anggota, Alumni serta Dosen mempunyai rasa kekeluargaan yang tinggi.</p>
+
+            <p><strong>MISI :</strong></p>
+            <p>1. Mengembangkan Keahlian Soft Skill dan Hard Skill Seluruh Anggota Himika dengan Mengikuti Dan Mengadakan Kegiatan Tingkat Regional,Dan National .</p>
+            <p>2. Membuat Suatu Wadah Atau Tempat Aspirasi,Sebagai Tempat Bertukar Fikiran,Penerapan Ilmu Kuliah, dan Pengakraban Diri Kepada Seluruh Penghuni Hima Guna Mempererat Kekeluargaan Hima.</p>
+            <br/>
+
+            <p> HIMIKA memiliki beragam kegiatan positif diantaranya adalah: </p>
+            <p> > PRO ( Pekanbaru Robotic Olimpiad ) merupakan salah satu kegiatan besar HIMIKA dalam bidang riset dan teknologi yaitu perlombaan robotika yang melibatkan siswa/i tingkatan SD, SMP, dan SMA/SMK se-Riau. </p>
+            <p> > LSE ( Lomba Seni Elektronika ) merupakan perlombaan dalam bidang seni yang menuntut kreatifitas mahasiswa Teknik Elektronika dalam pemanfaatan barang bekas menjadi barang yang berguna dan memiliki sentuhan elektronika.</p>
+            <p> > Pelatihan Robot Lego merupakan salah satu kegiatan HIMIKA dalam bidang pengabdian masyarakat dengan sasaran siswa/i Sekolah Dasar untuk meningkatkan minat dan bakat mereka dalam bidang teknologi khususnya robotika.</p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/prop.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Andrey Lahnan</h4>
+            <h5><strong>Gubernur ITSA</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/fikri1.jpg" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Fikri Ramadhan</h4>
+            <h5><strong>Wakil Gubernur ITSA</strong></h5>
+            
+          </div>
+        </div>
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand" href="index.html"><img src="assets/img/pcr1.png" alt="" /></a>
+              </div>
+              <p>
+                &copy; AJI Team - All right reserved
+              </p>
+              <div class="credits">
+
+                Created by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="scrollup"><i class="icon-chevron-up icon-square icon-48 active"></i></a>
+
+
+</body>
