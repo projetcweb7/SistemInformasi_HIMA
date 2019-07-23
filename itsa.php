4178527111696c1c317777296f1885020d5a9bb1
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-23 21:24:14 +0700

    Add files via upload
    File Upload ke 3

---
+++ itsa.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+  <!-- =======================================================
+    Theme Name: Vesperr
+    Theme URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+ <body>
+  <header>
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+        <img src="assets/img/pcr1.png" height="100px" width="100px" alt="" />
+        </a>
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li><a href="home.php" class="external">Beranda</a></li>
+                <li><a href="tentang.php" class="external">Tentang</a></li>
+                <li class="dropdown current">
+                  <a href="#">Himpunan<i class="icon-angle-down"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="itsa.php" class="external">ITSA</a></li>
+                    <li><a href="himasistifo.php" class="external">Himasistifo</a></li>
+                    <li><a href="himakom.php" class="external">Himakom</a></li>
+                    <li><a href="himika.php" class="external">Himika</a></li>
+                    <li><a href="aet.php" class="external">AET</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+           <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+
+            <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <img src="assets/img/dummies/itsa.jpg" height="100" width="100">
+            <h3><span>ITSA</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>
+               Information Technology Student Association Politeknik Caltex Riau (ITSA PCR) merupakan suatu organisasi kemahasiswaan yang beranggotakan seluruh mahasiswa/i jurusan Teknik Informatika  Politeknik Caltex Riau, himpunan ini pertama kali berdiri pada tanggal 16 Februari 2008 setelah berpisah dari Himpunan Mahasiswa Komputer. </p><br>
+            <p>  ITSA memiliki visi dan misi sebagai berikut : </p>
+            <p><strong>VISI :</strong></p>
+<p>
+Mewujudkan Information Technology Student Association Politeknik Caltex Riau sebagai pusat kegiatan mahasiswa di Provinsi Riau.</p>
+<p><strong>MISI :</strong></p>
+<p>
+Mengembangkan Information Technology Student Association Politeknik Caltex Riau dengan melakukan kerja sama dengan pihak dalam maupun pihak luar yang sifatnya tidak mengikat.
+Memonitori perkembangan teknologi informasi dan mengembangkan secara benar.
+Mempererat hubungan antar mahasiswa khususnya mahasiswa jurusan Teknik Informatika.</p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/Bgbil.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Khalifah Abil Jannatul Fasah</h4>
+            <h5><strong>Gubernur ITSA</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/bgbal5.jpg" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Iqbal Alhuda</h4>
+            <h5><strong>Wakil Gubernur ITSA</strong></h5>
+            
+          </div>
+        </div>
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end section about -->
+  <!-- end section contact -->
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand" href="index.html"><img src="assets/img/pcr1.png" alt="" /></a>
+              </div>
+              <p>
+                &copy; AJI Team - All right reserved
+              </p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Vesperr
+                -->
+                Created by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="scrollup"><i class="icon-chevron-up icon-square icon-48 active"></i></a>
+
+  <!-- Javascript Library Files -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.easing.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/parallax/jquery.parallax-1.1.3.js"></script>
+  <script src="assets/js/nagging-menu.js"></script>
+  <script src="assets/js/jquery.nav.js"></script>
+  <script src="assets/js/prettyPhoto/jquery.prettyPhoto.js"></script>
+  <script src="assets/js/portfolio/jquery.quicksand.js"></script>
+  <script src="assets/js/portfolio/setting.js"></script>
+  <script src="assets/js/hover/jquery-hover-effect.js"></script>
+  <script src="assets/js/jquery.scrollTo.min.js"></script>
+  <script src="assets/js/animate.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Custom Javascript File -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
