e16dda4cdfc9ffeb9ba8c5a614c8d48a8766fad4
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-23 22:24:22 +0700

    Update himasistifo.php
    Rubah file himasistifo.php

--- himasistifo.php
+++ himasistifo.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <title>Sistem Himpunan Mahasiswa</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="description" content="" />
   <meta name="author" content="" />
