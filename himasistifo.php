ebead23ab96b7a8457f706c742d4a562368515c8
atkhmi <atikah18ti@mahasiswa.pcr.ac.id>
2019-07-23 21:07:32 -0700

    Update himasistifo.php
    Update nama gub dan wagub himasistifo

--- himasistifo.php
+++ himasistifo.php
@@ -102,14 +102,14 @@
         <div class="span3">
           <img src="assets/img/dummies/prop.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
           <div class="roles">
-            <h4>Andrey Lahnan</h4>
+            <h4>Rio Arvindo</h4>
             <h5><strong>Gubernur HIMASISTIFO</strong></h5>
           </div>
         </div>
         <div class="span3">
           <img src="assets/img/dummies/fikri1.jpg" height="400px" alt="" class="img-polaroid" />
           <div class="roles">
-            <h4>Fikri Ramadhan</h4>
+            <h4>Abi Mahruf</h4>
             <h5><strong>Wakil Gubernur HIMASISTIFO</strong></h5>
             
           </div>
