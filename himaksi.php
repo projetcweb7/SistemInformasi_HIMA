72aaa1394f55d410518ba3c7c5ee6b1311a858ab
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-24 13:29:10 +0700

    create file himaksi.php
    Membuat file baru himaksi.php

---
+++ himaksi.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+  <!-- =======================================================
+    Theme Name: Vesperr
+    Theme URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+ <body>
+  <header>
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+        <img src="assets/img/pcr1.png" height="100px" width="100px" alt="" />
+        </a>
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li><a href="home.php" class="external">Beranda</a></li>
+                <li><a href="tentang.php" #about class="external">Tentang</a></li>
+                <li class="dropdown current">
+                  <a href="#">Himpunan<i class="icon-angle-down"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="itsa.php" class="external">ITSA</a></li>
+                    <li><a href="himasistifo.php" class="external">Himasistifo</a></li>
+                    <li><a href="himakom.php" class="external">Himakom</a></li>
+                    <li><a href="himika.php" class="external">Himika</a></li>
+                    <li><a href="aet.php" class="external">AET</a></li>
+                    <li><a href="himatel.php" class="external">Himatel</a></li>
+                    <li><a href="himaksi.php" class="external">Himaksi</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+           <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+
+            <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <img src="assets/img/dummies/himaksi.jpg" height="100" width="100">
+            <h3><span>Himaksi</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>HIMAKSI merupakan himpunan mahasiswa yang berasal dari Program studi Akuntansi yang berdiri pada tahun 2003 oleh generasi pertama mahasiswa akuntansi Politeknik Caltex Riau. Terbentuknya himpunan mahasiswa akuntansi ini bertujuan agar memperat hubungan silaturrahim antar mahasiswa akuntansi khususnya dan mahasiswa antar program studi serta untuk memperat hubungan mahasiswa akuntansi lain antar universitas se-Riau. </p><br>
+            <p>Yang menjadi dasar terbentuknya himaksi yaitu agar adanya suatu wadah atau tempat mahasiswa akuntansi saling bertukar pikiran dan berdiskusi mengenai kegiata-kegiatan yang berhubungan dengan  akuntansi pada khususnya. Selain itu himaksi juga merupakan suatu organisasi yang dapat menampung inspirasi dan inspirasi mahasiswanya untuk suatu kemajuan dan sekaligus melatih mahasiswa-mahasiswa agar memiliki jiwa kepemimpinan, kebersamaan, serta melatih kedisiplinan.</p><br/>
+            <p>Adapun berbagai kegiatan yang dilakukan oleh himaksi merupakan suatu kesepakatan yang dilakukan oleh mahasiswa-mahasiswa akuntansi yang dilakukan secara musyawarah. Himaksi telah berjalan beberapa tahun dan ini merupakan keberhasilan dari mahasiswa-mahasiswa akuntansi yag memiliki potensi sehingga dapat membangun Himaksi menjadi organisasi yang baik di Politeknik Caltex Riau. Himaksi di bentuk memiliki tujuan agar mahasiswa-mahasiswa akuntansi dapat mengenal bagaimana dunia keorganisasian dan menimbulkan rasa tanggungjawab didiri mereka masing-masing terhadap tugas-tugas yang diberikan.</p><br/>
+            <p> Adapun Kegiatan Himpunan Mahasiswa Akuntansi antara lain : </p>
+            <p>1. Latihan Dasar Organisasi.</p>
+            <p>2. HIMAKSI Day.</p>
+            <p>3. Sekolah Pasar Modal.</p>
+            <p>4. Festival Akuntansi.</p>
+            <p>5. Inter High School Accounting (IHSAO).</p>
+            <p>6. Musyawarah Besar HIMAKSI.</p>
+            <p>7. Masa Gabung Mahasiswa Akuntansi (MAGMA).</p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/prop.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Rio Arvindo</h4>
+            <h5><strong>Gubernur HIMASISTIFO</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/fikri1.jpg" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Abi Mahruf</h4>
+            <h5><strong>Wakil Gubernur HIMASISTIFO</strong></h5>
+            
+          </div>
+        </div>
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end section about -->
+  <!-- end section contact -->
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand" href="index.html"><img src="assets/img/pcr1.png" alt="" /></a>
+              </div>
+              <p>
+                &copy; AJI Team - All right reserved
+              </p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Vesperr
+                -->
+                Created by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="scrollup"><i class="icon-chevron-up icon-square icon-48 active"></i></a>
+
+  <!-- Javascript Library Files -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.easing.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/parallax/jquery.parallax-1.1.3.js"></script>
+  <script src="assets/js/nagging-menu.js"></script>
+  <script src="assets/js/jquery.nav.js"></script>
+  <script src="assets/js/prettyPhoto/jquery.prettyPhoto.js"></script>
+  <script src="assets/js/portfolio/jquery.quicksand.js"></script>
+  <script src="assets/js/portfolio/setting.js"></script>
+  <script src="assets/js/hover/jquery-hover-effect.js"></script>
+  <script src="assets/js/jquery.scrollTo.min.js"></script>
+  <script src="assets/js/animate.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Custom Javascript File -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
