d8de12e3f3b1d72c4867c1e3fc0c2c1c4c5f1aa7
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-07-24 19:55:31 +0700

    Update home.php

--- home.php
+++ home.php
@@ -127,7 +127,6 @@ Perubahan sistem kampus dari Akademi menjadi Politeknik merupakan salah satu hal
           <div class="roles">
             <h5><strong>AET</strong></h5>
           </div>
-        </div>
         <!-- Bawah -->
          <div class="row">
         <div class="span3">
