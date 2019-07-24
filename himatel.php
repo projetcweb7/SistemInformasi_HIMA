d6d0f39ef62eec710315c444d60e8cce1220725b
atkhmi <atikah18ti@mahasiswa.pcr.ac.id>
2019-07-24 12:38:27 -0700

    Create himatel.php
    Menambahkan HIMATEL

---
+++ himatel.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sistem Informasi Himpunan Mahasiswa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+
+  
+<!--  ============================================ -->
+  
+  
+  
+</head>
+ <body>
+  <header>
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+        <img src="assets/img/pcr1.png" height="100px" width="100px" alt="" />
+        </a>
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li><a href="home.php" class="external">Beranda</a></li>
+                <li><a href="tentang.php" class="external">Tentang</a></li>
+                <li class="dropdown current">
+                  <a href="#">Himpunan<i class="icon-angle-down"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="itsa.php" class="external">ITSA</a></li>
+                    <li><a href="himasistifo.php" class="external">Himasistifo</a></li>
+                    <li><a href="himakom.php" class="external">Himakom</a></li>
+                    <li><a href="himika.php" class="external">Himika</a></li>
+                    <li><a href="aet.php" class="external">AET</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+           <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+
+            <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <img src="assets/img/dummies/Aet.jpg" height="100" width="100">
+            <h3><span>HIMATEL</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>Himpunan Mahasiswa Telekomunikasi (HIMATEL) merupakan himpunan mahasiswa Progran Studi Teknik Elektronika yang bernaung di kampus Politeknik Caltex Riau yang berdiri sejak tahun 2001. HIMATEL adalah himpunan pertama kali yang ada di Politeknik Caltex Riau atau dengan kata lain HIMATEL merupakan pencetus organisasi pertama yang berada di Politeknik Caltex Riau.  Himpunan ini dipimpin oleh seorang GUBERNUR, yang mempunyai kewajiban penuh terhadap tercapainya program-program kerja  selama 1 (satu) periode pada setiap tahunnya.</p><br>
+
+            <p><strong>VISI : </strong></p>
+            <p>1. Meningkatkan keimanan dan ketakwaan terhadap tuhan yang maha esa</p>
+            <p>2. Meningkatkan kebersamaan dan kekeluargaan antar mahasiswa program studi teknik telekomunikasi</p>
+            <p>3. Meningkatkan kemampuan bersaing dan daya kreatifitas mahasiswa teknik telekomunikasi</p>
+
+            <p><strong>MISI :</strong></p>
+            <p>1. Mengadakan dan mengikuti kegiatan keagamaan yang berguna bagi mahasiswa program studi teknik telekomunikasi</p>
+            <p>2. Melakukan koordinasi dan silaturrahmi secara terpadu dan kontiniu antar sesama mahasiswa program studi teknik telekomunikasi</p>
+            <p>3. Mengadakan dan  mengikuti kegiatan pelatihan peningkatan skill dan kreatifitas mahasiswa program studi teknik telekomunikasi</p>
+            
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/prop.jpg"  width="400px" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Andrey Lahnan</h4>
+            <h5><strong>Gubernur ITSA</strong></h5>
+          </div>
+        </div>
+        <div class="span3">
+          <img src="assets/img/dummies/fikri1.jpg" height="400px" alt="" class="img-polaroid" />
+          <div class="roles">
+            <h4>Fikri Ramadhan</h4>
+            <h5><strong>Wakil Gubernur ITSA</strong></h5>
+            
+          </div>
+        </div>
+        <div class="span3">
+          <div class="roles">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end section about -->
+  <!-- end section contact -->
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand" href="index.html"><img src="assets/img/pcr1.png" alt="" /></a>
+              </div>
+              <p>
+                &copy; AJI Team - All right reserved
+              </p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Vesperr
+                -->
+                Created by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="scrollup"><i class="icon-chevron-up icon-square icon-48 active"></i></a>
+
+  <!-- Javascript Library Files -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.easing.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/parallax/jquery.parallax-1.1.3.js"></script>
+  <script src="assets/js/nagging-menu.js"></script>
+  <script src="assets/js/jquery.nav.js"></script>
+  <script src="assets/js/prettyPhoto/jquery.prettyPhoto.js"></script>
+  <script src="assets/js/portfolio/jquery.quicksand.js"></script>
+  <script src="assets/js/portfolio/setting.js"></script>
+  <script src="assets/js/hover/jquery-hover-effect.js"></script>
+  <script src="assets/js/jquery.scrollTo.min.js"></script>
+  <script src="assets/js/animate.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Custom Javascript File -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
