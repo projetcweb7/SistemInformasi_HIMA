5b74c96ccd5d59770fd6a1274cc38730c6532e2a
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2019-08-05 10:50:34 +0700

    Update himakom.php
    Nambah

--- himakom.php
+++ himakom.php
@@ -87,7 +87,9 @@
             <p>1. Menyelenggarakan Sistem Pendidikan Profesional di bidang Teknik Komputer dan Teknologi Informasi yang Menghasilkan Sumber Daya Manusia, Barang (software dan hardware) dan Jasa (konsultasi ) yang berkualitas.</p>
             <p>2. Menghasilkan lulusan yang profesional/terampil dan ahli di bidang Teknik Komputer dan Teknologi Informasi, berpikir terbuka (open minded) serta siap bersaing di pasar global berdasarkan kompetensi dunia Industri pada tingkat Nasional maupun Internasional dengan menyediakan sebuah lingkungan belajar yang baik bagi mahasiswa.</p>
             <p>3. Ikut berperan aktif dalam pengembangan asosiasi profesi di bidang komputer dan teknologi informasi yang menunjang kelancaran hubungan institusional dengan masyarakat industri. Mengembangkan dan menerapkan nilai-nilai etika, moral agama dan moral akademis.</p>
-            <p>4. Mengembangkan Riset terapan yang terkait dengan bidang komputer dan Teknologi Informasi untuk melayani kebutuhan industri yang meliputi kebutuhan sumber daya manusia, konsultasi teknis dan penelitian.</p>
+            <p>4. Mengembangkan Riset terapan yang terkait dengan bidang komputer dan Teknologi Informasi untuk melayani kebutuhan industri yang meliputi kebutuhan sumber daya manusia, konsultasi teknis dan penelitian.</p> <br/>
+
+            <h1> Ini Yang baru ditambahkan </h1>
                     </div>
         </div>
       </div>
